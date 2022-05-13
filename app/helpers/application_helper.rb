16b3a37a98fb7af55bb8cab1998c1244a0bf3fe5
EC2 Default User <ec2-user@ip-172-31-46-202.ap-northeast-1.compute.internal>
2022-05-13 18:18:58 +0000

    トップページに入れた

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def full_title(page_name="")
+    base_title="授業紹介アプリ"
+    if page_name.empty?
+      base_title
+    else
+      page_name+"|"+base_title
+    end
+  end
+
 end
