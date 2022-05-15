ef2250fa710fc6ebc18be71dce954723f8291673
EC2 Default User <ec2-user@ip-172-31-46-202.ap-northeast-1.compute.internal>
2022-05-15 03:00:22 +0000

    seedsのエラー解消

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,8 +5,8 @@ class Post < ApplicationRecord
     User.find(self.user_id)
   end
   
-  validates :university, presence:true
   validates :title, presence:true
   validates :description, presence:true
+  
   
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,62 +1,63 @@
-User.create!(name: "管理人",
-             email: "sample@email.com",
-             password: "password",
-             password_confirmation: "password" ,
-             admin:true)
-             
-User.create!(name: "ゲスト管理人",
-             email: "guest_admin@email.com",
-             password: "password",
-             password_confirmation: "password" ,
-             admin:true)
+User.create!( name: "管理人",
+              email: "sample@email.com",
+              password: "password",
+              password_confirmation: "password",
+              admin:true)
 
-User.create!(name: "ゲストユーザー",
-             email: "guest@email.com",
-             password: "password",
-             password_confirmation: "password")
-             
-User.create!(name: "大学生1",
-             email: "sampleA@email.com",
-             password: "password",
-             password_confirmation: "password" ,
-             uni:true)
+User.create!( name: "ゲスト管理人",
+              email: "guest_admin@email.com",
+              password: "password",
+              password_confirmation: "password",
+              admin:true)
 
-User.create!(name: "大学生2",
-             email: "sampleB@email.com",
-             password: "password",
-             password_confirmation: "password" ,
-             uni:true)
+User.create!( name: "ゲストユーザー",
+              email: "guest@email.com",
+              password: "password",
+              password_confirmation: "password")
 
-User.create!(name: "高校生1",
-             email: "sample-1@email.com",
-             password: "password",
-             password_confirmation: "password" ,
-             high:true)
-             
+User.create!( name: "大学生1",
+              email: "sampleA@email.com",
+              password: "password",
+              password_confirmation: "password",
+              uni:true)
 
-User.create!(name: "高校生2",
-             email: "sample-2@email.com",
-             password: "password",
-             password_confirmation: "password" ,
-             high:true)
+User.create!( name: "大学生2",
+              email: "sampleB@email.com",
+              password: "password",
+              password_confirmation: "password",
+              uni:true)
+
+User.create!( name: "高校生1",
+              email: "sample-1@email.com",
+              password: "password",
+              password_confirmation: "password",
+              high:true)
 
 
-Post.create!(title: "マクロ経済A",
-            description: "財政政策や金融政策の効果について学びます",
-            user_id:4)
 
-Post.create!(title: "微分積分",
-            description: "数学を使って、経済モデルを分析します",
-            user_id:4)
+
+Post.create!(university: "A大学",
+             department: "経済学部",
+             branch: "経済学科",
+             title: "マクロ経済A",
+             description: "財政政策や金融政策の効果について学びます",
+             user_id:4)
+
+Post.create!(university: "A大学",
+             department: "経済学部",
+             branch: "経済学科",
+             title: "微分積分",
+             description: "数学を使って、経済モデルを分析します",
+             user_id:4)
             
 
-Post.create!(title: "ミクロ経済A",
-            description: "消費者や企業の行動ついて学びます",
-            user_id:5)
-            
+Post.create!(university: "B大学",
+             department: "経済学部",
+             branch: "経済学科",
+             title: "ミクロ経済A",
+             description: "消費者や企業の行動ついて学びます",
+             user_id:5)
 
 
-puts "Users Created"
-
-puts "Posts Created"
-
+puts "Users Created!"
+puts "Posts Created!"
