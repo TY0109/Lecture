4be32a440fe05ed0cac6e95628b95a66b6ae1560
矢木敏樹 <yagitoshiki@yagitoshikinoMacBook-Air.local>
2022-05-31 19:37:23 +0900

    mailのvali消してみた

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,9 +13,7 @@ class User < ApplicationRecord
   validates :name,  presence: true, length: { maximum: 50 }
   
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 100 },
-                    format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: true
+  
   
   validates :student, presence:true              
   validates :password, presence: true, length: { minimum: 6}, allow_nil: true
