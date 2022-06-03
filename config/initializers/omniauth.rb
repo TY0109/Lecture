2180102ad94958bb88062d3b1bb04ced65e76f8a
矢木敏樹 <yagitoshiki@yagitoshikinoMacBook-Air.local>
2022-06-03 15:51:06 +0900

    post get

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
     require 'omniauth/strategies/line'
     provider :line, ENV['LINE_KEY'], ENV['LINE_SECRET']
+    
+    OmniAuth.config.allowed_request_methods = [:post, :get]
+    
 end
