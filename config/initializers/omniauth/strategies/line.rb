9985aad1d3847301ddd82745af84f3cc6f367d48
矢木敏樹 <yagitoshiki@yagitoshikinoMacBook-Air.local>
2022-06-03 19:07:50 +0900

    cookie

--- config/initializers/omniauth/strategies/line.rb
+++ config/initializers/omniauth/strategies/line.rb
@@ -35,8 +35,8 @@ module OmniAuth
       # nonceをリクエストパラメータに追加するためoverride
       def authorize_params
         super.tap do |params|
-          params[:nonce] = SecureRandom.uuid
-          session["omniauth.nonce"] = params[:nonce]
+          params[:state] = SecureRandom.uuid
+          cookie["omniauth.state"] = params[:state]
           
           params[:client_id] = 1657162701
           session["omniauth.client_id"] = params[:client_id]
