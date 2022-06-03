fb984c4b9bee30a25b2457d75937a3881fecd472
矢木敏樹 <yagitoshiki@yagitoshikinoMacBook-Air.local>
2022-06-03 16:14:50 +0900

    sessions c作成

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,14 @@
+class SessionsController < ApplicationController
+    skip_before_action :verify_authenticity_token, only: :create
+
+    def create
+      p "auth_hash", auth_hash
+      p "info", auth_hash.info
+    end
+  
+    private
+  
+    def auth_hash
+      request.env['omniauth.auth']
+    end
+end
